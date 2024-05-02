83ab20dafffb6b05023ac904af2067816b3be112
Prerna Shakwar <98837965+prerna-shakwar@users.noreply.github.com>
2024-05-02 22:24:23 +0530

    Update FoldersCreation.py

--- FoldersCreation.py
+++ FoldersCreation.py
@@ -1,9 +1,5 @@
-# Importing the Libraries Required
-
 import os
 import string
-
-# Creating the directory Structure
 
 if not os.path.exists("dataSet"):
     os.makedirs("dataSet")
@@ -14,15 +10,12 @@ if not os.path.exists("dataSet/trainingData"):
 if not os.path.exists("dataSet/testingData"):
     os.makedirs("dataSet/testingData")
 
-# Making folder  0 (i.e blank) in the training and testing data folders respectively
 for i in range(0):
     if not os.path.exists("dataSet/trainingData/" + str(i)):
         os.makedirs("dataSet/trainingData/" + str(i))
 
     if not os.path.exists("dataSet/testingData/" + str(i)):
         os.makedirs("dataSet/testingData/" + str(i))
-
-# Making Folders from A to Z in the training and testing data folders respectively
 
 for i in string.ascii_uppercase:
     if not os.path.exists("dataSet/trainingData/" + i):
